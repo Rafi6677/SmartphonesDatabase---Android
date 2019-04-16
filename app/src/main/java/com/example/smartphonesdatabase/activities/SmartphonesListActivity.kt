084cdaf3aa3dab635f2a8772a908b4f5b5f81c3d
Rafał Giemza <rafalgiemza6677@gmail.com>
2019-04-16 17:41:29 +0200

    Checking few items in adapter, deleting data from Firebase db

--- app/src/main/java/com/example/smartphonesdatabase/activities/SmartphonesListActivity.kt
+++ app/src/main/java/com/example/smartphonesdatabase/activities/SmartphonesListActivity.kt
@@ -1,10 +1,17 @@
 package com.example.smartphonesdatabase.activities
 
+import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.util.SparseIntArray
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.example.smartphonesdatabase.R
 import com.example.smartphonesdatabase.models.Smartphone
 import com.google.firebase.database.DataSnapshot
@@ -17,8 +24,12 @@ import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_smartphones_list.*
 import kotlinx.android.synthetic.main.smartphones_list_row.view.*
 import java.util.*
+import kotlin.collections.HashMap
 
 class SmartphonesListActivity : AppCompatActivity() {
+
+    val adapter = GroupAdapter<ViewHolder>()
+    var smartphonesToDelete = HashMap<String, Boolean>()
 
     companion object {
         val SMARTPHONE_KEY = "smartphoneKey"
@@ -38,65 +49,107 @@ class SmartphonesListActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        var isReadyToDelete = false
+
         when (item?.itemId) {
+
             R.id.addSmartphone -> {
                 val intent = Intent(this, AddDataActivity::class.java)
                 startActivity(intent)
             }
             R.id.deleteSmartphone -> {
+                smartphonesToDelete.values.forEach {
+                    if(it)
+                        isReadyToDelete = true
+                }
 
+                if(isReadyToDelete) {
+                    AlertDialog.Builder(this)
+                        .setTitle("UWAGA!")
+                        .setMessage("Zamierzasz usunąć rekordy z bazy danych. Kontynuować?")
+                        .setPositiveButton("OK", DialogInterface.OnClickListener { dialog, which ->
+                            for((key, value) in smartphonesToDelete) {
+                                if(value) {
+                                    val ref = FirebaseDatabase.getInstance().getReference("smartphones/$key")
+                                    ref.removeValue()
+                                        .addOnSuccessListener {
+                                            println("usunieto")
+                                        }
+                                        .addOnFailureListener {
+                                            println("nie usunieto")
+                                        }
+                                }
+                            }
+                            adapter.clear()
+                            showData()
+                        })
+                        .setNegativeButton("Anuluj", DialogInterface.OnClickListener { dialog, which ->
+                            adapter.clear()
+                            showData()
+                        })
+                        .show()
+                }
+                else {
+                    Toast.makeText(this, "Brak zaznaczonych elementów.", Toast.LENGTH_SHORT).show()
+                }
             }
         }
 
         return super.onOptionsItemSelected(item)
     }
 
-    private fun setupData() {
-        val id = UUID.randomUUID().toString()
-        val brand = "Huawei"
-        val model = "P9"
-        val systemVersion = "Android 7.0"
-        val website = "https://www.telepolis.pl/telefony/huawei/p9"
-
-        val ref = FirebaseDatabase.getInstance().getReference("/smartphones/$id")
-        val smartphone = Smartphone(id, brand, model, systemVersion, website)
-
-        ref.setValue(smartphone)
-            .addOnSuccessListener {
-                println("git")
-            }
-            .addOnFailureListener {
-                println("nie git")
-            }
-    }
-
     private fun showData() {
         val ref = FirebaseDatabase.getInstance().getReference("/smartphones")
+        smartphonesToDelete.clear()
 
         ref.addListenerForSingleValueEvent(object: ValueEventListener{
             override fun onDataChange(p0: DataSnapshot) {
-                val adapter = GroupAdapter<ViewHolder>()
-
                 p0.children.forEach {
                     val smartphone = it.getValue(Smartphone::class.java)
                     if (smartphone != null) {
                         adapter.add(SmartphoneItem(smartphone))
+                        smartphonesToDelete[smartphone.id] = false
                     }
                 }
-
-                adapter.setOnItemClickListener { item, view ->
-                    val smartphoneItem = item as SmartphoneItem
-
-                    val intent = Intent(view.context, EditDataActivity::class.java)
-                    intent.putExtra(SMARTPHONE_KEY, smartphoneItem.smartphone)
-                    startActivity(intent)
-                }
-
-                recyclerView_SmartphonesList.adapter = adapter
             }
 
             override fun onCancelled(p0: DatabaseError) { }
         })
+
+        adapter.setOnItemClickListener { item, view ->
+            val smartphoneItem = item as SmartphoneItem
+
+            val intent = Intent(view.context, EditDataActivity::class.java)
+            intent.putExtra(SMARTPHONE_KEY, smartphoneItem.smartphone)
+            startActivity(intent)
+        }
+
+        adapter.setOnItemLongClickListener { item, view ->
+            val smartphoneItem = item as SmartphoneItem
+            val id = smartphoneItem.smartphone.id
+
+            var color = 0
+            val currentBackgroundCollor = view.background
+            if(currentBackgroundCollor is ColorDrawable) {
+                color = currentBackgroundCollor.color
+            }
+
+            if(color == resources.getColor(R.color.notSelected)) {
+                view.setBackgroundColor(resources.getColor(R.color.selected))
+
+                smartphonesToDelete[id] = true
+            }
+            else {
+                view.setBackgroundColor(resources.getColor(R.color.notSelected))
+
+                smartphonesToDelete[id] = false
+            }
+
+            println(smartphonesToDelete)
+            item.isLongClickable
+        }
+
+        recyclerView_SmartphonesList.adapter = adapter
     }
 }
 
